c8ba534b3e213e2f0fb3a908404b5ca0b3433943
naveenlingala <naveenlingala1212@gmail.com>
2023-03-04 11:22:46 +0530

    profile-bills

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics',blank=True, null=True)
-
+    # abc
 
     def profile_posts(self):
         return self.user.post_set.all()
