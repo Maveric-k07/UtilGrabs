8eb065530424efda359ed1b96f2ae87e8e28fc44
naveenlingala <naveenlingala1212@gmail.com>
2023-03-04 13:09:15 +0530

    add-billss

--- users/models.py
+++ users/models.py
@@ -3,17 +3,34 @@ from django.contrib.auth.models import User
 
 """ Model for User Profile """
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_online = models.BooleanField(default=False)
-    following = models.ManyToManyField(User, related_name="following", blank=True)
-    friends = models.ManyToManyField(User, related_name='my_friends', blank=True)
-    bio = models.CharField(default="",blank=True,null=True,max_length=350)
-    date_of_birth = models.CharField(blank=True,max_length=150)
+    following = models.ManyToManyField(
+        User, related_name="following", blank=True)
+    friends = models.ManyToManyField(
+        User, related_name='my_friends', blank=True)
+    bio = models.CharField(default="", blank=True, null=True, max_length=350)
+    date_of_birth = models.CharField(blank=True, max_length=150)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics',blank=True, null=True)
-    # abc
+    image = models.ImageField(default='default.jpg',
+                              upload_to='profile_pics', blank=True, null=True)
+    current_bill = models.FloatField(null=True)
+    water_bill = models.FloatField(null=True)
+
+    def has_current_bill(self):
+        print(self.current_bill)
+        if not self.current_bill:
+            return True
+        return False
+
+    def has_water_bill(self):
+        print(self.water_bill)
+        if not self.water_bill:
+            return True
+        return False
 
     def profile_posts(self):
         return self.user.post_set.all()
@@ -28,19 +45,20 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 
-
 STATUS_CHOICES = (
-    ('send','send'),
-    ('accepted','accepted')
+    ('send', 'send'),
+    ('accepted', 'accepted')
 )
 
+
 class Relationship(models.Model):
-    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='friend_sender')
-    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='friend_receiver')
+    sender = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='friend_sender')
+    receiver = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='friend_receiver')
     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.sender}-{self.receiver}-{self.status}"
-
